c9b8e11a245d722df6a943a590f2d4b4b3497b56
Bernhard Danecker <bernhard.danecker@troii.com>
2018-01-16 11:42:50 +0100

    added initial project

---
+++ timrAPITestClient/ClientForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace TimrSyncTest
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.ListBox();
+            this.txtChanged = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIdentifier = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get cars";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // result
+            // 
+            this.result.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.result.FormattingEnabled = true;
+            this.result.Location = new System.Drawing.Point(12, 119);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(643, 420);
+            this.result.TabIndex = 1;
+            // 
+            // txtChanged
+            // 
+            this.txtChanged.Location = new System.Drawing.Point(189, 81);
+            this.txtChanged.Name = "txtChanged";
+            this.txtChanged.Size = new System.Drawing.Size(100, 20);
+            this.txtChanged.TabIndex = 2;
+            this.txtChanged.Validating += new System.ComponentModel.CancelEventHandler(this.txtChanged_Validating);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 79);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Get work times";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "last modified:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Get users";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Identifier:";
+            // 
+            // txtIdentifier
+            // 
+            this.txtIdentifier.Location = new System.Drawing.Point(68, 6);
+            this.txtIdentifier.Name = "txtIdentifier";
+            this.txtIdentifier.Size = new System.Drawing.Size(100, 20);
+            this.txtIdentifier.TabIndex = 6;
+            this.txtIdentifier.TextChanged += new System.EventHandler(this.txtIdentifier_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Authentication token:";
+            // 
+            // txtToken
+            // 
+            this.txtToken.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtToken.Location = new System.Drawing.Point(288, 6);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(367, 20);
+            this.txtToken.TabIndex = 8;
+            this.txtToken.TextChanged += new System.EventHandler(this.txtToken_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 552);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIdentifier);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtChanged);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "timr API sample client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox result;
+        private System.Windows.Forms.TextBox txtChanged;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIdentifier;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtToken;
+    }
+}
+
---
+++ timrAPITestClient/ClientForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TimrSyncTest.TimrSyncWebService;
+
+namespace TimrSyncTest
+{
+    public partial class ClientForm : Form
+    {
+        private TimrSyncService timr = null;
+
+        public ClientForm()
+        {
+            InitializeComponent();
+
+            DateTime now = DateTime.Now;
+            txtChanged.Text = new DateTime(now.Year, now.Month, 1).ToShortDateString();
+        }
+
+        private TimrSyncService Timr
+        {
+            get
+            {
+                if (timr == null)
+                {
+                    if (String.IsNullOrEmpty(txtIdentifier.Text) || String.IsNullOrEmpty(txtToken.Text))
+                        throw new Exception("Identifier or authentication token are missing (could be generated in you timr account settings)");
+
+                    timr = new TimrSyncService();
+                    timr.Url = @"https://timrsync.timr.com/timr/timrsync";
+                    timr.Credentials = new System.Net.NetworkCredential(txtIdentifier.Text, txtToken.Text);
+                }
+                return timr;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                result.Items.Clear();
+                foreach (Car car in Timr.GetCars(new Object()))
+                {
+                    result.Items.Add(car.name);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void txtChanged_Validating(object sender, CancelEventArgs e)
+        {
+            DateTime result;
+            e.Cancel = !DateTime.TryParse(txtChanged.Text, out result);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                result.Items.Clear();
+                WorkTimeQuery query = new WorkTimeQuery();
+
+                DateTime? date = GetLastModified();
+                if (date.HasValue)
+                    query.lastModified = date.Value;
+                query.lastModifiedSpecified = date.HasValue;
+
+                foreach (WorkTime workTime in Timr.GetWorkTimes(query))
+                {
+                    result.Items.Add(workTime.lastModifiedTime + ", " + workTime.description);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private DateTime? GetLastModified()
+        {
+            if (!String.IsNullOrEmpty(txtChanged.Text))
+            {
+                DateTime lastModified = DateTime.Parse(txtChanged.Text);
+                DateTime lastModifiedUTC = new DateTime(lastModified.Year, lastModified.Month, lastModified.Day, lastModified.Hour, lastModified.Minute, lastModified.Second, DateTimeKind.Unspecified);
+
+                return lastModifiedUTC;
+            }
+            return null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                result.Items.Clear();
+                foreach (User user in Timr.GetUsers(new Object()))
+                {
+                    result.Items.Add(user.firstname + " " + user.lastname);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void txtIdentifier_TextChanged(object sender, EventArgs e)
+        {
+            timr = null;
+        }
+
+        private void txtToken_TextChanged(object sender, EventArgs e)
+        {
+            timr = null;
+        }
+    }
+}
